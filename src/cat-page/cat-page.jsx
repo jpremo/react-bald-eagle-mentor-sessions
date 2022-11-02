d580006e90be465c1e159b516cafe17434669aa9
Jacob Premo <jpremo@forio.com>
2022-11-01 21:01:58 -0400

    20221101

--- src/cat-page/cat-page.jsx
+++ src/cat-page/cat-page.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { getCatImage, getCatBreed } from '../utils';
+import './cat-page.css';
 
 const CatPage = () => {
+    // The code on line 10 is a more concise version of lines 7-9
+    // const catStuff = React.useState('');
+    // const catUrl = catStuff[0]
+    // const setCatUrl = catStuff[1]
+    const [catUrls, setCatUrls] = React.useState([]);
+    const fetchRandomCat = async() => {
+        const newCatUrl = await getCatImage(getCatBreed());
+        setCatUrls([...catUrls, newCatUrl]);
+    }
     return (
         <div className='cat-page'>
             I am a page for cats
+            {catUrls.map((catUrl, idx) => {
+                return (
+                    <img src={catUrl} key={`cat-url-${idx}`}/>
+                );
+            })}
+            <button onClick={fetchRandomCat}>
+                Get Random Cat Picture
+            </button>
         </div>
     );
 };
