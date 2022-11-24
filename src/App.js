8313648c1fc8b38f9582f68f1802be06e0d307c5
Jacob Premo <jpremo@forio.com>
2022-11-23 21:06:04 -0500

    20221123

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+
+  const [name, setName] = useState('');
+  const [familyName, setFamilyName] = useState('');
+
+  useEffect(() => {
+    console.log('useEffect!')
+  }, [])
+
+  console.log('rendering');
+
+  const handleNameChange = (event) => {
+    const numbers = '0123456789';
+    const newValue = event.target.value;
+    const lastCharacter = newValue[newValue.length -1];
+    //Filtering out numbers to prevent user from inputting numbers into the name field
+    if(numbers.includes(lastCharacter)) {
+      console.log(`${lastCharacter} is not permitted to be entered into the name field!`)
+    } else {
+      setName(event.target.value)
+    }
+  }
+
+  const handleFamilyNameChange = (event) => {
+    setFamilyName(event.target.value)
+  }
+
   return (
     <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <h1>useEffect Example</h1>
+      <input value={name} onChange={handleNameChange} placeholder="Given Name"/>
+      <input value={familyName} onChange={handleFamilyNameChange} placeholder="Family Name"/>
     </div>
   );
 }
