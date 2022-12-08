e2065fc65ca874dcddf0351576fb0f5aa9b13270
Jacob Premo <jpremo@forio.com>
2022-12-07 21:14:33 -0500

    Fixed console warning

--- src/random-cat-card/random-cat-card.jsx
+++ src/random-cat-card/random-cat-card.jsx
@@ -4,29 +4,32 @@ import { getRandomCatImage } from '../utils';
 const RandomCatCard = ({ name }) => {
     const [description, setDescription] = useState(null);
     const [catUrl, setCatUrl] = useState(null);
-    useEffect(async () => {
-        //Async/await example
-        const catSummary = await getRandomCatImage();
-        setDescription(catSummary.description);
-        setCatUrl(catSummary.url);
-        // Below is an example of using the fetch API with promises
-        // const res = fetch(
-        //   `https://api.thecatapi.com/v1/images/search?breed_ids=asho`,
-        //   {
-        //       headers: {
-        //           "Content-Type": "application/json",
-        //           "x-api-key": "b07ebe63-3b58-4285-a374-10aa6a74e158",
-        //       },
-        //       method: "GET",
-        //   }
-        // ).then((catResponse) => {
-        //   console.log('catResponse (fetch result after processing)', catResponse)
-        //   catResponse.json().then((data) => {
-        //     console.log('final cat data', data);
-        //   });
-        // }).catch((e) => {
-        //   console.log('we have an error', e)
-        // });
+    useEffect(() => {
+        const getCatData = async () => {
+            //Async/await example
+            const catSummary = await getRandomCatImage();
+            setDescription(catSummary.description);
+            setCatUrl(catSummary.url);
+            // Below is an example of using the fetch API with promises
+            // const res = fetch(
+            //   `https://api.thecatapi.com/v1/images/search?breed_ids=asho`,
+            //   {
+            //       headers: {
+            //           "Content-Type": "application/json",
+            //           "x-api-key": "b07ebe63-3b58-4285-a374-10aa6a74e158",
+            //       },
+            //       method: "GET",
+            //   }
+            // ).then((catResponse) => {
+            //   console.log('catResponse (fetch result after processing)', catResponse)
+            //   catResponse.json().then((data) => {
+            //     console.log('final cat data', data);
+            //   });
+            // }).catch((e) => {
+            //   console.log('we have an error', e)
+            // });
+        }
+        getCatData();
     }, []);
 
     //Alternative to the ternary below
