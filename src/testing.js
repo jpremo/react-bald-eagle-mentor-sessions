2ecec409de87d5d62e7d05a6d0ad3427088c5dba
Jacob Premo <jpremo@forio.com>
2022-12-13 11:30:57 -0500

    Setup

---
+++ src/testing.js
@@ -0,0 +1,9 @@
+//what is a promise
+
+//how to create a promise
+
+//how to chain a promise and why that's necessary
+
+//using async await to make promise based code more readable
+
+//how does the fetch API use promises?
