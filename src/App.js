4b97e862a4b632b5fc71df0729247c639deef97c
Jacob Premo <jpremo@forio.com>
2022-12-14 13:34:09 -0500

    setup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
 
+//discussion on debugging
+
+//checking the console
+  //reading error messages
+  //using console.log to view values
+    //making console.log more evident
+      // \n vs console.log('%c ', 'font-size: 20px; color: #FB15B9FF;', );
+
+//debugging tool
+
+//what if no errors in console?
+  //look at html structure
+  //look at react devtools
+  //look at network tab
 function App() {
   return (
     <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
